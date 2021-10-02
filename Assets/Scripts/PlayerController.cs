ae888d03c6c30c4076590796b8b1210be4c34ded
Krzysztof <mail@ravenmore.pl>
2021-10-02 11:29:28 +0100

    Added TextMeshPro. Rewired controls to use InputManager and physics.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float moveHorizontal;
+    private float moveVertical;
+
     public float MovementSpeed = 1f;
 
-    public float RotationSpeed = 10f;
-
-    public float MaxRotationAngle = 60f;
-
-    public float ResetSpeed = 10f;
-
-    public float MaximumAcceleration = 5;
-
-    public float AccelerationRate = 1;
-    public float HorizontalLevelLimit = 7.5f;
-    public float VerticalLevelLimit = 3f;
+  
 
     public ParticleSystem ps;
+    public Animator PlayerAnimator;
+    public TextMeshPro text;
+    public Rigidbody2D PlayerRigidbody;
+    public float TiltSpeed = 100;
+    public float MaxTilt;
 
     private float Acceleration = 0;
+    private float Tilt = 0;
+    private Vector2 movement;
     
     private enum Direction{None,Left,Right,Up,Down};
 
@@ -34,50 +36,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Quaternion currentRotation = transform.rotation;
-        if(Input.GetKey(KeyCode.A))
-        {
-            if (DirectionVariable != Direction.Left) Acceleration = 0;
-            DirectionVariable = Direction.Left;
-            transform.position += Time.deltaTime * (MovementSpeed+Acceleration) * Vector3.left;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, MaxRotationAngle, 0), Time.deltaTime*RotationSpeed);
-            if (DirectionVariable == Direction.Left) Acceleration += AccelerationRate*Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {   if (DirectionVariable != Direction.Right) Acceleration = 0;
-            DirectionVariable = Direction.Right;
-            transform.position -= Time.deltaTime * (MovementSpeed+Acceleration) * Vector3.left;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -MaxRotationAngle, 0), Time.deltaTime*RotationSpeed);
-            if (DirectionVariable == Direction.Right) Acceleration += AccelerationRate*Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            if (DirectionVariable != Direction.Up) Acceleration = 0;
-            DirectionVariable = Direction.Up;
-            transform.position -= Time.deltaTime * (MovementSpeed + Acceleration) * Vector3.down;
-            if (DirectionVariable == Direction.Right) Acceleration += AccelerationRate * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            if (DirectionVariable != Direction.Down) Acceleration = 0;
-            DirectionVariable = Direction.Down;
-            transform.position -= Time.deltaTime * (MovementSpeed + Acceleration) * Vector3.up;
-            if (DirectionVariable == Direction.Right) Acceleration += AccelerationRate * Time.deltaTime;
-        }
-        else
-        {
-            Acceleration -= AccelerationRate * Time.deltaTime;
-            DirectionVariable = Direction.None;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime*ResetSpeed);
-        }
 
-        //clamp accelleration, don't let the player leave the game area
-        Acceleration = Mathf.Clamp(Acceleration, 0,MaximumAcceleration);
-        if (transform.position.x < -HorizontalLevelLimit) transform.position = new Vector3(-HorizontalLevelLimit, transform.position.y, transform.position.z);
-        if (transform.position.x > HorizontalLevelLimit) transform.position = new Vector3(HorizontalLevelLimit, transform.position.y, transform.position.z);
-        if (transform.position.y < -VerticalLevelLimit) transform.position = new Vector3(transform.position.x, -VerticalLevelLimit, transform.position.z);
-        if (transform.position.y > VerticalLevelLimit) transform.position = new Vector3(transform.position.x, VerticalLevelLimit, transform.position.z);
-
+        moveHorizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        moveVertical = Input.GetAxis("Vertical") * MovementSpeed;
+        movement = new Vector2(moveHorizontal, moveVertical);
+        PlayerRigidbody.velocity = movement;
 
     }
 
